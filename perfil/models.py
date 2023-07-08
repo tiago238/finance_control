cc84218b698c14f7e2e30936ab29cf8d38583554
Tiago Martins <tiago_martins238@hotmail.com>
2023-07-08 01:44:33 -0300

    ver planejamento

--- perfil/models.py
+++ perfil/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from django.db.models import Sum
 
 class Categoria(models.Model):
     categoria = models.CharField(max_length=50)
@@ -7,7 +9,18 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.categoria
-
+    def total_gasto(self):
+        from extrato.models import Valores
+        valores = Valores.objects.filter(categoria__id = self.id).filter(data__month = datetime.now().month).filter(tipo='S')
+        #TODO: Tentar implantar -> total = valores.aggregate(Sum('valor'))['valor__sum']
+        total_valor = 0
+        for valor in valores:
+            total_valor += valor.valor
+        return total_valor
+    
+    def calcula_percentual_gasto_por_categoria(self):
+        return int((self.total_gasto() * 100) / self.valor_planejamento)
+    
 class Conta(models.Model):
     banco_choices = (
         ('NU', 'Nubank'),
--- planejamento/views.py
+++ planejamento/views.py
@@ -18,3 +18,8 @@ def update_valor_categoria(request, id):
       return JsonResponse({'status': 'Sucesso'})
     except:
       return JsonResponse({'status': 'Erro'})
+    
+def ver_planejamento(request):
+    categorias = Categoria.objects.all()
+    #TODO: Realizar barra com total
+    return render(request, 'ver_planejamento.html', {'categorias': categorias})
